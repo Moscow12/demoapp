3407d48e893d613f3278ebea9baf0b1e81b2b6c1
Moscow12 <meshackstev@gmail.com>
2024-09-26 01:31:59 +0300

    website page with livewire

---
+++ app/Livewire/ProductdetailPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ProductdetailPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.productdetail-page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\HomePage;
+use App\Livewire\MyOrdersDetailPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductdetailPage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class);  
+Route::get('categories', CategoriesPage::class);
+Route::get('products', ProductsPage::class);
+Route::get('cart', CartPage::class);
+Route::get('/products/{product}', ProductdetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/myorderspage', MyOrdersPage::class);
+Route::get('/myorders/{order}', MyOrdersDetailPage::class);
