3ea237999c3c0d276609b2d353d3a5f73451307b
Moscow12 <meshackstev@gmail.com>
2024-10-01 23:20:31 +0300

    progress

--- app/Livewire/ProductdetailPage.php
+++ app/Livewire/ProductdetailPage.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+#[Title('Details -DEMO')]
 
 class ProductdetailPage extends Component
 {
+    public $slug;
+
+   
     public function render()
     {
-        return view('livewire.productdetail-page');
+        return view('livewire.productdetail-page', [
+            'product'=>Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
+    }
+
+     public function mount($slug){
+       
+        $this->slug=$slug;
     }
 }
--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Product DEMO APP')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $ProductQuery = Product::query()->where('is_active', 1);
+        $brands=Brand::where('is_active', 1)->get('id', 'name', 'slug');
+        $categories = Category::where('is_active', 1)->get('id', 'name', 'slug');
+        return view('livewire.products-page',[
+            'products'=>$ProductQuery->paginate(6),
+            'brands'=>$brands,
+            'categories'=>$categories
+        ]);
     }
 }
